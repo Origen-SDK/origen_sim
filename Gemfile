80d689160e28b1daf63250246d379f187fb18ca9
Stephen McGinty <stephen.f.mcginty@gmail.com>
2018-01-18 14:14:12 +0000

    Moved the org file generation infrastructure into Origen core

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'origen_jtag'
 #gem 'ruby-debug-ide'
 #gem 'debase'
 #gem "origen", path: "~/Code/github/origen"
-gem "origen", git: "https://github.com/Origen-SDK/origen.git", branch: "rtl_pins"
+gem "origen", git: "https://github.com/Origen-SDK/origen.git", branch: "org_infrastructure"
 #gem "origen", git: "https://github.com/Origen-SDK/origen.git"
 #gem "origen_testers", path: "~/Code/github/origen_testers"
 #gem "origen_verilog", path: "~/Code/github/origen_verilog"
--- lib/origen_sim/origen_testers/api.rb
+++ lib/origen_sim/origen_testers/api.rb
@@ -16,7 +16,11 @@ module OrigenTesters
       pins = pins.map { |p| p.is_a?(String) || p.is_a?(Symbol) ? dut.pin(p) : p }
       pins.each(&:save)
       @sim_capture = pins.map { |p| [p, "origen.dut.#{p.rtl_name}"] }
-      with_capture_file(id) do
+      Origen::OrgFile.open(id) do |org_file|
+        @org_file = org_file
+        if update_capture?
+          @sim_capture.each { |pin, net| pin.record_to_org_file(only: :assert) }
+        end
         yield
       end
       pins.each(&:restore)
@@ -32,23 +36,17 @@ module OrigenTesters
         cycles.times do
           if update_capture?
             _origen_testers_cycle(options)
-            l = ''
             @sim_capture.each do |pin, net|
-              l += "#{pin.id},assert,b#{simulator.peek(net)};"
+              pin.assert(simulator.peek(net))
+              # Remove the assertion since it is for the previous cycle in terms of the current simulation,
+              # this won't be captured to the org file
+              pin.dont_care
             end
-            if @capture_buffer
-              if l == @capture_buffer
-                @capture_buffer_cycles += 1
-              else
-                capture_file.puts "#{@capture_buffer}#{@capture_buffer_cycles}"
-                @capture_buffer = l
-                @capture_buffer_cycles = 1
-              end
-            else
-              @capture_buffer = l
-              @capture_buffer_cycles = 1
-            end
+            Origen::OrgFile.cycle
           else
+            unless @org_file.exist?
+              fail "The simulation capture \"#{id}\" has not been made yet, re-run this pattern with a simulation target first!"
+            end
             apply_captured_data
             _origen_testers_cycle(options)
           end
@@ -58,61 +56,22 @@ module OrigenTesters
       end
     end
 
-    def with_capture_file(id)
-      filename = "#{capture_dir}/#{id}.org"
-      @capture_present = File.exist?(filename)
-      if update_capture?
-        File.open(filename, 'w') do |f|
-          @capture_file = f
-          yield
-          capture_file.puts "#{@capture_buffer}#{@capture_buffer_cycles}"
-        end
-      else
-        unless File.exist?(filename)
-          fail "The simulation capture \"#{id}\" has not been made yet, re-run this pattern with a simulation target first!"
-        end
-        File.open(filename, 'r') do |f|
-          @capture_file = f
-          yield
-        end
-      end
-      @capture_file = nil
-    end
-
     def apply_captured_data
       if @apply_captured_data_cycles && @apply_captured_data_cycles > 1
         @apply_captured_data_cycles -= 1
       else
-        read_capture_line do |operations, cycles|
+        @org_file.read_line do |operations, cycles|
           @apply_captured_data_cycles = cycles
-          operations.each do |pin_id, operation, data|
-            dut.pin(pin_id).send(operation, Origen::Value.new(data))
+          operations.each do |object, operation, *args|
+            object.send(operation, *args)
           end
         end
       end
     end
 
-    def read_capture_line
-      l = @capture_file.readline.strip.split(';')
-      cycles = l.pop.to_i
-      yield l.map { |l| l.split(',') }, cycles
-    end
-
     def update_capture?
       return @update_capture if defined? @update_capture
-      @update_capture = sim? && (!@capture_present || Origen.app!.update_sim_captures)
-    end
-
-    def capture_file
-      @capture_file
-    end
-
-    def capture_dir
-      @capture_dir ||= begin
-        d = "#{Origen.root}/pattern/sim_capture/#{Origen.target.name}/"
-        FileUtils.mkdir_p(d)
-        d
-      end
+      @update_capture = sim? && (!@org_file.exist? || Origen.app!.update_sim_captures)
     end
   end
 end
